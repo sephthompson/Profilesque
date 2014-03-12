891eab0ac7e11d14cfddf4dfead8176c08267c19
sephthompson <sephthompson@gmail.com>
2014-03-12 19:55:10 -0400

    Call it a day

--- src/com/example/profilesque/Profile.java
+++ src/com/example/profilesque/Profile.java
@@ -4,6 +4,7 @@ import com.example.profilesque.R;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class Profile extends Activity {
 	
@@ -11,5 +12,12 @@ public class Profile extends Activity {
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.profile);
+		
+		TextView txtFN = (TextView) findViewById(R.id.textView1);
+		TextView txtLN = (TextView) findViewById(R.id.textView2);
+		TextView txtAB = (TextView) findViewById(R.id.textView3);
+		TextView txtFNDISP = (TextView) findViewById(R.id.textView4);
+		TextView txtLNDISP = (TextView) findViewById(R.id.textView5);
+		TextView txtABDISP = (TextView) findViewById(R.id.textView6);
 	}
 }
